4bfcf3cc6dd821fca03577525fc2a8f0974cb4fa
LeoSirius <is.li.xiaoyu@qq.com>
2020-06-23 18:35:22 +0800

    update

---
+++ mdn教程demos/js/promise.js
@@ -0,0 +1,3 @@
+// 参考: 
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
