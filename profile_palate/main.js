40b289216c43a8ba158782517ca7c5fee5cb70f3
LeoSirius <is.li.xiaoyu@qq.com>
2020-09-10 16:41:06 +0800

    update

--- profile_palate/main.js
+++ profile_palate/main.js
@@ -1,5 +1,7 @@
 
 var toolContainer = document.getElementById('toolContainer');
+var whiteBg = document.getElementById('whiteBg');
+console.log(whiteBg)
 
 
 var angle = 0;
@@ -7,13 +9,28 @@ var angle = 0;
 var toggleArrowClass = function(e) {
   angle += 180;
   e.target.style.transform = 'rotate(' + angle + 'deg)';
-  console.log('e.target.style.transform = ', e.target.style.transform)
   e.target.style.transition = 'all 0.5s';
 
+
   if (e.target.classList.contains('arrow')) {
+    // remove arrow and white bg and icons
+
+    e.target.style.background = "url(./icons/three-lines.png) no-repeat";
+    e.target.style.backgroundSize = "100%";
     e.target.classList.remove('arrow');
+
+    whiteBg.style.opacity = "0";
   } else {
+    // show arrow and white bg and icons
+
+    e.target.style.background = "url(./icons/arrow.png) no-repeat";
+    e.target.style.backgroundSize = "100%";
     e.target.classList.add('arrow');
+
+    whiteBg.style.opacity = "0.8";
   }
+
 };
+
+
 toolContainer.addEventListener('click', toggleArrowClass);
