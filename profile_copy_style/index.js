c5fb9aac474c781ca3c1507559bb529d9d61d4f9
LeoSirius <is.li.xiaoyu@qq.com>
2020-09-15 18:32:39 +0800

    update

--- profile_copy_style/index.js
+++ profile_copy_style/index.js
@@ -1,10 +1,12 @@
 var iconMenu = document.getElementById('icon-container-menu');
 var profileBG = document.getElementById('profile-bg');
+var profileContent = document.getElementById('profile-content');
 
 
 iconMenu.onclick = function(e) {
   e.currentTarget.classList.toggle('change');
   profileBG.classList.toggle('change');
+  profileContent.classList.toggle('change');
 }
 
 
