577b7261bc1f3f2f095855bec3f70a2d4b73e42b
LeoSirius <is.li.xiaoyu@qq.com>
2020-09-16 16:39:15 +0800

    update

--- profile_copy_style/index.js
+++ profile_copy_style/index.js
@@ -1,13 +1,20 @@
 var iconMenu = document.getElementById('icon-container-menu');
 var profileBG = document.getElementById('profile-bg');
 var profileContent = document.getElementById('profile-content');
+var profileFooter = document.getElementById('profile-footer');
 
 
 iconMenu.onclick = function(e) {
-  e.currentTarget.classList.toggle('change');
+  iconMenu.classList.toggle('change');
   profileBG.classList.toggle('change');
   profileContent.classList.toggle('change');
-}
+  profileFooter.classList.toggle('change');
 
+  if (profileContent.classList.contains('change')) {
+    // show profile
+  } else {
+    // hide profile
+  }
+}
 
 iconMenu.addEventListener('click', handleClickMenu);
